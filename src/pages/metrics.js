98e4b435a6ed6a860156d712e8778a68b8b57830
Tomas <btomas@fi.uba.ar>
2022-11-26 21:26:57 -0300

    Avances de metricas

--- src/pages/metrics.js
+++ src/pages/metrics.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 import { Box, Container, Grid, Typography } from '@mui/material';
 import { Budget } from '../components/dashboard/budget';
 import { LatestOrders } from '../components/dashboard/latest-orders';
-// import { LatestProducts } from '../components/dashboard/latest-products';
+import { LatestProducts } from '../components/dashboard/latest-products';
 import { Sales } from '../components/dashboard/sales';
 import { TasksProgress } from '../components/dashboard/tasks-progress';
 import { TotalPassengers } from '../components/dashboard/total-passengers';
@@ -49,8 +49,8 @@ const Page = () => (
           </Grid> */}
           <Grid
             item
-            xl={3}
-            lg={3}
+            xl={4}
+            lg={4}
             sm={6}
             xs={12}
           >
@@ -58,8 +58,8 @@ const Page = () => (
           </Grid>
           <Grid
             item
-            xl={3}
-            lg={3}
+            xl={4}
+            lg={4}
             sm={6}
             xs={12}
           >
@@ -67,30 +67,12 @@ const Page = () => (
           </Grid>
           <Grid
             item
-            xl={3}
-            lg={3}
+            xl={4}
+            lg={4}
             sm={6}
             xs={12}
           >
             <TotalAdmins />
-          </Grid>
-          <Grid
-            item
-            xl={3}
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            {/* <TasksPrTaskogress /> */}
-          </Grid>
-          <Grid
-            item
-            xl={3}
-            lg={3}
-            sm={6}
-            xs={12}
-          >
-            <TotalProfit sx={{ height: '100%' }} />
           </Grid>
           <Grid
             item
@@ -99,7 +81,25 @@ const Page = () => (
             xl={9}
             xs={12}
           >
-            {/* <Sales /> */}
+            {/* <TasksProgress /> */}
+          </Grid>
+          <Grid
+            item
+            xl={3}
+            lg={3}
+            sm={6}
+            xs={12}
+          >
+            {/* <TotalProfit sx={{ height: '100%' }} /> */}
+          </Grid>
+          <Grid
+            item
+            lg={8}
+            md={12}
+            xl={9}
+            xs={12}
+          >
+            <Sales />
           </Grid>
           <Grid
             item
@@ -108,7 +108,7 @@ const Page = () => (
             xl={3}
             xs={12}
           >
-            {/* <TrafficByDevice sx={{ height: '100%' }} /> */}
+            <TrafficByDevice sx={{ height: '100%' }} />
           </Grid>
           <Grid
             item
