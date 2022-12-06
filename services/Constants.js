435b19d33eb0ac4e0f38b1ccc508b815981034e0
Tomas <btomas@fi.uba.ar>
2022-12-06 16:04:19 -0300

    Fixed

--- services/Constants.js
+++ services/Constants.js
@@ -4,4 +4,4 @@ import { currentUserToken } from '../src/contexts/currentAdmin';
 export const URL_USERS = `${CONFIG.gatewayURL}${CONFIG.usersBasePath}`;
 export const URL_TRIPS = `${CONFIG.gatewayURL}${CONFIG.tripsBasePath}`//`${CONFIG.gatewayURL}${CONFIG.tripsBasePath}`;
 export const URL_AUTH = `${CONFIG.gatewayURL}${CONFIG.authBasePath}`;
-export const HEADERS = { headers: { Accept: 'application/json', 'auth-token': currentUserToken.token.toString()}}; 
+export const HEADERS = { headers: { Accept: 'application/json', 'auth-token': currentUserToken.token}}; 
