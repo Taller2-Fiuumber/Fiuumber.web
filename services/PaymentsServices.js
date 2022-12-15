45c69bf598f49a27ed028b95772ffaaee18a6f7b
acai <acai@fi.uba.ar>
2022-12-15 16:46:28 -0300

    Add loadBallanceToWallet

--- services/PaymentsServices.js
+++ services/PaymentsServices.js
@@ -13,9 +13,7 @@ export const PaymentsServices = {
     loadBallanceToWallet: async (amountInEthers, receiverAddress) => {
         try {
             const url = `${URL_PAYMENTS}/depositToReceiver`;
-            const response = await axios.post(url, {amountInEthers: amountInEthers.toString(), receiverAddress}, getHeaders(),);
-            const { hash } = response.data;
-            return hash;
+            return await axios.post(url, {amountInEthers: amountInEthers.toString(), receiverAddress: receiverAddress}, getHeaders(),);
         }
         catch (error) {
             if (error.code === "UNPREDICTABLE_GAS_LIMIT") {
@@ -28,24 +26,5 @@ export const PaymentsServices = {
             }
         }
     },
-
-    loadBallanceToWallet: async (amountInEthers, walletAddress) => {
-        try {
-            const response = await axios.post(`${URL_PAYMENTS}/depositToReceiver`, {amountInEthers: amountInEthers.toString(), walletAddress}, PaymentsServices.getHeaders(),);
-            return response.data;
-        }
-        catch (error) {
-            if (error.code === "UNPREDICTABLE_GAS_LIMIT") {
-                console.log(`Transaction was not successful, please retry`);
-                return await loadBallanceToWallet(amountInEthers, walletAddress);
-            } else {
-                console.log(`Load Balance to wallet: ${error}`);
-                if (error && error.response && error.response.status == 401) return undefined;
-                return undefined;
-            }
-        }
-    },
-
-
 
 };
