f6953a41cdd65dd7643671fdf3d763ea341904a1
Sofia Gualdieri <sgualdieri@fi.uba.ar>
2022-12-16 23:25:51 -0300

    fix

--- services/Constants.js
+++ services/Constants.js
@@ -5,4 +5,4 @@ export const URL_PAYMENTS = `${CONFIG.gatewayURL}${CONFIG.paymentsBasePath}`;
 export const URL_USERS = `${CONFIG.gatewayURL}${CONFIG.usersBasePath}`;
 export const URL_TRIPS = `${CONFIG.gatewayURL}${CONFIG.tripsBasePath}`//`${CONFIG.gatewayURL}${CONFIG.tripsBasePath}`;
 export const URL_AUTH = `${CONFIG.gatewayURL}${CONFIG.authBasePath}`;
-// export const HEADERS = { headers: { Accept: 'application/json', 'auth-token': currentUserToken.token}}; 
+export const HEADERS = { headers: { accept: 'application/json', 'auth-token': currentUserToken.token}}; 
